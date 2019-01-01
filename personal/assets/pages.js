e6cf8b5ce57ddac4056ed4435f95be495d712741
David Gundry <david@gundry.co.uk>
2019-01-01 10:46:40 +0000

    General improvements

--- personal/assets/pages.js
+++ personal/assets/pages.js
@@ -1,90 +1,73 @@
-function internalLinkClicked(anchor, event)
-{
-    updateSection(anchor.getAttribute("href").substring(1), event);   
+function internalLinkClicked(anchor, event) {
+    updateSection(anchor.getAttribute("href").substring(1), event);
 }
 
-function updateSection(section, e)
-{
-    if (section)
-    {
+function updateSection(section, e) {
+    if (section) {
         var topLinks = document.querySelector("nav").getElementsByTagName("a");
-        for (var i = 0; i < topLinks.length; i++)
-        {
+        for (var i = 0; i < topLinks.length; i++) {
             if (topLinks[i].getAttribute("href").substr(1) === section)
-                topLinks[i].parentElement.style.backgroundColor = "#9999a1";
-            else
-            {
+                topLinks[i].parentElement.style.backgroundColor = "#61707d";
+            else {
                 topLinks[i].parentElement.style.backgroundColor = "";
             }
-                
+
         }
     }
 
-    if (e)
-    {
+    if (e) {
         e.preventDefault();
         window.location = "#" + section;
     }
     var list = document.querySelector("main").getElementsByTagName("section");
     var visibleElement = false;
-    for (var i = 0; i < list.length; i++)
-    {
+    for (var i = 0; i < list.length; i++) {
         list[i].hidden = true;
-        if (list[i].id === section)
-        {
+        if (list[i].id === section) {
             list[i].hidden = false;
             visibleElement = true;
 
             var leftButton = document.querySelector("#sideButton_left")
-            if (leftButton)
-            {
-                if (i > 0)
-                {
-                    leftButton.href = "#"+list[i-1].id;
+            if (leftButton) {
+                if (i > 0) {
+                    leftButton.href = "#" + list[i - 1].id;
                     leftButton.hidden = false;
                 }
-                else
-                {
+                else {
                     leftButton.href = "";
                     leftButton.hidden = true;
                 }
             }
             var rightButton = document.querySelector("#sideButton_right")
-            if (rightButton)
-            {
-                if (i < list.length-1)
-                {
-                    rightButton.href = "#"+list[i+1].id;
+            if (rightButton) {
+                if (i < list.length - 1) {
+                    rightButton.href = "#" + list[i + 1].id;
                     rightButton.hidden = false;
                 }
-                else
-                {
+                else {
                     rightButton.href = "";
                     rightButton.hidden = true;
                 }
             }
         }
     }
-    if ((!visibleElement) && (list.length > 0))
-    {
+    if ((!visibleElement) && (list.length > 0)) {
         updateSection(list[0].id)
-    }    
+    }
 }
 
 
 
-function createSideButton(parent, content, side)
-{
+function createSideButton(parent, content, side) {
     var button = document.createElement("a");
-    button.id = "sideButton_"+side;
-    button.style.cssText = "float: " + side + "; color: grey; font-size: 60pt; margin: 0; text-decoration: none;";
+    button.id = "sideButton_" + side;
+    button.style.cssText = "float: " + side + "; color: #61707d; font-size: 60pt; margin: 0; text-decoration: none;";
     button.addEventListener('click', internalLinkClicked.bind(null, button), false);
     button.appendChild(document.createTextNode(content));
     parent.appendChild(button);
 }
 
-function createSideButtons()
-{
+function createSideButtons() {
     var container = document.createElement("div");
     container.id = "sideButtons";
     container.style.cssText = "position: fixed; left: 50%; top: 50%; width: 100%; min-width: 580px; max-width: 800px; transform: translate(-50%,-50%);"
@@ -94,55 +77,57 @@ function createSideButtons()
     main.appendChild(container);
 }
 
-function mediaMatchChanged(mediaMatch)
-{
+function mediaMatchChanged(mediaMatch) {
     if (mediaMatch.matches)
         document.querySelector("#sideButtons").hidden = false;
     else
         document.querySelector("#sideButtons").hidden = true;
 }
 
+function createLinks()
+{
+    var list = document.querySelector("main").getElementsByTagName("section");
+    for (var i=0;i<list.length;i++)
+    {
+        if (i < list.length-1)
+        {
+            var child = document.createElement('a');
+            child.innerHTML = "Next >";
+            child.href = "#"+list[i+1].id;
+            child.style.cssFloat="right";
+            list[i].appendChild(child);//, list[i].firstChild);
+        }
+
+        if (i > 0)
+        {
+            var child = document.createElement('a');
+            child.innerHTML = "< Back";
+            child.href = "#"+list[i-1].id;
+            child.style.cssFloat="left";
+            list[i].appendChild(child);//, list[i].firstChild);
+        }
+        
+
+    }
+}
+
+
+
+var h = window.location.hash;
+updateSection(h ? h.substr(1) : "", null);
+createLinks();
+
+/*
 var mediaMatch = window.matchMedia("(min-width: 580px)") // 480 + 
 createSideButtons();
 mediaMatchChanged(mediaMatch);
 mediaMatch.addListener(mediaMatchChanged); // Attach listener function on state changes 
-
+*/
 var a = document.getElementsByTagName("a");
 for (var i = 0; i < a.length; i++) {
-    if (a[i].hasAttribute("href"))
-    {
+    if (a[i].hasAttribute("href")) {
         var href = a[i].getAttribute("href");
         if (href && href[0] === "#")
             a[i].addEventListener('click', internalLinkClicked.bind(null, a[i]), false);
     }
 }
-
-var h = window.location.hash;
-updateSection(h ? h.substr(1) : "", null);
-
-if (window.innerWidth < 400)
-{
-    
-}
-
-
-function nameMediaMatchChanged(mediaMatch)
-{
-    if (mediaMatch.matches)
-    {
-        let n = document.querySelector("body>nav>ul>li>a");
-        n.innerHTML="DG";
-        n.style.fontSize = "110%";
-    }
-    else
-    {
-        let n = document.querySelector("body>nav>ul>li>a");
-        n.innerHTML="DAVID GUNDRY";
-        n.style.fontSize = "100%";
-        n.style.margin = "0"
-    }
-}
-
-var mediaMatch = window.matchMedia("(max-width: 360px)")
-nameMediaMatchChanged(mediaMatch);
-mediaMatch.addListener(nameMediaMatchChanged); 
