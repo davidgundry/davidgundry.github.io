e275390f0c98104ad8863212ed8a970564712282
David Gundry <david@gundry.co.uk>
2019-01-23 11:27:11 +0000

    Remove commented code

--- _site/assets/pages.js
+++ _site/assets/pages.js
@@ -30,64 +30,12 @@ function updateSection(section, e) {
         if (list[i].id === section) {
             list[i].hidden = false;
             visibleElement = true;
-
-            /*
-            var leftButton = document.querySelector("#sideButton_left")
-            if (leftButton) {
-                if (i > 0) {
-                    leftButton.href = "#" + list[i - 1].id;
-                    leftButton.hidden = false;
-                }
-                else {
-                    leftButton.href = "";
-                    leftButton.hidden = true;
-                }
-            }
-            var rightButton = document.querySelector("#sideButton_right")
-            if (rightButton) {
-                if (i < list.length - 1) {
-                    rightButton.href = "#" + list[i + 1].id;
-                    rightButton.hidden = false;
-                }
-                else {
-                    rightButton.href = "";
-                    rightButton.hidden = true;
-                }
-            }*/
         }
     }
     if ((!visibleElement) && (list.length > 0)) {
         updateSection(list[0].id)
     }
 }
-
-
-/*
-function createSideButton(parent, content, side) {
-    var button = document.createElement("a");
-    button.id = "sideButton_" + side;
-    button.style.cssText = "float: " + side + "; color: #61707d; font-size: 60pt; margin: 0; text-decoration: none;";
-    button.addEventListener('click', internalLinkClicked.bind(null, button), false);
-    button.appendChild(document.createTextNode(content));
-    parent.appendChild(button);
-}
-
-function createSideButtons() {
-    var container = document.createElement("div");
-    container.id = "sideButtons";
-    container.style.cssText = "position: fixed; left: 50%; top: 50%; width: 100%; min-width: 580px; max-width: 800px; transform: translate(-50%,-50%);"
-    createSideButton(container, "<", "left");
-    createSideButton(container, ">", "right");
-    var main = document.querySelector("main");
-    main.appendChild(container);
-}
-
-function mediaMatchChanged(mediaMatch) {
-    if (mediaMatch.matches)
-        document.querySelector("#sideButtons").hidden = false;
-    else
-        document.querySelector("#sideButtons").hidden = true;
-}*/
 
 function createLinks()
 {
@@ -124,12 +72,6 @@ var h = window.location.hash;
 updateSection(h ? h.substr(1) : "", null);
 createLinks();
 
-/*
-var mediaMatch = window.matchMedia("(min-width: 580px)") // 480 + 
-createSideButtons();
-mediaMatchChanged(mediaMatch);
-mediaMatch.addListener(mediaMatchChanged); // Attach listener function on state changes 
-*/
 var a = document.getElementsByTagName("a");
 for (var i = 0; i < a.length; i++) {
     if (a[i].hasAttribute("href")) {
